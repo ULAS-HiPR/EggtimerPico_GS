25c518b4c3ecc75a9e0314412771c3eedd4df2e5
Derv6464 <dervlag6464@gmail.com>
2025-09-14 20:09:02 +0100

    adding more error messages

--- main.py
+++ main.py
@@ -4,32 +4,48 @@ from ble_server import BLESimplePeripheral
 from machine import Pin, UART
 import gps_parser
 import struct
+
     
-tx = Pin(4)
-rx = Pin(5)
+tx = Pin(0)
+rx = Pin(1)
 
 
     
-uart = UART(1, 9600, tx=tx, rx=rx)       
+uart = UART(0, 9600, tx=tx, rx=rx)       
 ble = bluetooth.BLE()
 p = BLESimplePeripheral(ble)
+file_name = 'gps_data.txt'
 
 time.sleep(3)
     
+
 gps = gps_parser.GPSReader(uart)
 
-loop = 0
+data = []
+time_since_fix = time.time()
+time_since_save = time.time()
+last_data = 'no data'
 while True:
     
+    #raw = uart.read()
+    #if raw:
+    #    print(raw.decode())
+    #else:
+    #    print('none')
+    try:
+        gps_data = gps.get_data()
+    
+        if (gps_data.latitude) or (gps_data.longitude):
+            location_data = str(gps_data.latitude).encode('utf-8') + " " + str(gps_data.longitude).encode('utf-8')
+            last_data = location_data
+            time_since_fix = time.time()
+        sending_data = last_data + ', ' + str(time.time() - time_since_fix)
+        p.send(sending_data)
+        print(sending_data)
 
-    gps_data = gps.get_data()
-    
-    print(gps_data.has_fix, gps_data.latitude, gps_data.longitude)
-    sending_data = str(gps_data.latitude).encode('utf-8') + " " + str(gps_data.longitude).encode('utf-8')
-    
-    p.send(sending_data)
-    print(sending_data, loop)
-    loop += 1
+    except:
+        pass
+        
     time.sleep(0.5)
 
         
